7b40c76e9ca73080532988d1c382ea0729d7654f
nqhgithubtest1 <nqh.github.test.1@gmail.com>
2024-10-04 19:59:20 +0700

    Client call API

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,26 @@
+﻿void DongBo()
+{
+    Console.WriteLine("Bat dau cong viec 1...");
+    Task.Delay(5000).Wait();
+    Console.WriteLine("Ket thuc cong viec 1.");
+}
+
+async void BatDongBo()
+{
+    Console.WriteLine("Bat dau cong viec 1...");
+    await Task.Delay(5000);
+    Console.WriteLine("Ket thuc cong viec 1.");
+}
+
+async void SomeMethod()
+{
+    Console.WriteLine("Some Method Started......");
+    await Task.Delay(TimeSpan.FromSeconds(10));
+    Console.WriteLine("\n");
+    Console.WriteLine("Some Method End");
+}
+
+Console.WriteLine("Bat dau cong viec 2...");
+BatDongBo();
+Console.WriteLine("Ket thuc cong viec 2.");
+Console.ReadKey();
---
+++ Client/Controllers/CarsController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NET105_Bai5;
+using Newtonsoft.Json;
+
+namespace Client.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public CarsController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7275/api/cars");
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var cars = JsonConvert.DeserializeObject<List<Car>>(result);
+                return View(cars);
+            }
+            else
+            {
+                return Content("Error");
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7275/api/cars/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var car = JsonConvert.DeserializeObject<Car>(result);
+                return View(car);
+            }
+            else
+            {
+                return Content("Error");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Car car)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(car),
+                System.Text.Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("https://localhost:7275/api/cars", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var response = await _httpClient.GetAsync("https://localhost:7275/api/cars/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                var result = await response.Content.ReadAsStringAsync();
+                var car = JsonConvert.DeserializeObject<Car>(result);
+                return View(car);
+            }
+            else
+            {
+                return Content("Error");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Car car)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(car),
+                System.Text.Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync("https://localhost:7275/api/cars", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var response = await _httpClient.DeleteAsync("https://localhost:7275/api/cars/" + id);
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return Content("Error");
+            }
+        }
+    }
+}
